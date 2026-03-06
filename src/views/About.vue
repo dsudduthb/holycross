7c952c18324a08012acdc5eb1ba341598117b899
dsudduthb <contactdanielbillingsley@gmail.com>
2026-03-06 17:44:19 -0600

    update(about page creed): made an alternate creed card with an image.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import CREED from '../assets/creed.txt?raw'
+import creed_image from '../assets/priest_kneeling_mass.png'
 
 var show_creed = ref(false);
 
@@ -25,6 +26,24 @@ function expandCreed(){
                 <v-card v-if="show_creed" class="ml-3 mr-3 mb-3 pa-2" :rounded="0" style="font-family: 'Times New Roman', Times, serif;">
                     {{ CREED }}
                 </v-card>
+            </v-card>
+        </div>
+
+        <div class="pt-5">
+            <v-card color="primary">
+                <v-row no-gutters>
+                <v-col cols="6">
+                    <v-img :src="creed_image" cover height="100%"/>
+                </v-col>
+                <v-col cols="6">
+                    <div class="pt-3" style="font-family: Georgia, 'Times New Roman', Times, serif; font-weight: bold; text-align: center;">
+                        THE NICENE CREED
+                    </div>
+                    <div  class="pl-3 pr-3 pb-3" style="font-family: 'Times New Roman', Times, serif; text-align: justify;">
+                        "{{CREED}}"
+                    </div>
+                </v-col>
+                </v-row>
             </v-card>
         </div>
     </div>
