e2832bc0f71185b1196ea3d3a622249a9d8f859d
dsudduthb <contactdanielbillingsley@gmail.com>
2026-03-10 20:04:22 -0500

    add(home): mass schedule banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,8 +9,8 @@
   }
 </script>
 <template>
-  <div>
 
+<v-container style="max-width: 50%;">
     <div class="mt-10 mb-10">
       <h1 class="title">Traditional Anglicanism</h1>
       <h2 class="subtext">in Oklahoma City</h2>
@@ -32,7 +32,7 @@
           <v-btn @click="goToWorship()"
             class="mt-10 mb-10" 
             color="white" 
-            style="font-family: Georgia, 'Times New Roman', Times, serif; {left: '50%', transform:'translateX(-50%)'}">
+            style="font-family: Georgia, 'Times New Roman', Times, serif;">
             Join Us in Worship
           </v-btn>
 
@@ -44,7 +44,38 @@
 
         </div>
       </v-card>
-    </div>
+      
 
-  </div>
+    </div>  
+  </v-container>
+    <v-container fluid class="px-0">
+      <v-card class="mt-10 w-100 text-center pa-10" color="primary">
+        <v-row justify="center" class="pa-10">
+          <v-col cols="auto">
+            <div class="schedule-header">Morning Prayer</div>
+            <div class="schedule-times">Sunday - 9:30 AM</div>
+          </v-col>
+          <v-col cols="auto">
+            <div class="schedule-header">Holy Eucharist</div>
+            <div class="schedule-times">Sunday - 10:00 AM</div>
+          </v-col>
+          <v-col cols="auto">
+            <div class="schedule-header">Evening Prayer</div>
+            <div class="schedule-times">Thursday - 6:00 PM</div>
+          </v-col>
+        </v-row>
+      </v-card>
+  </v-container>
 </template>
+<style>
+.schedule-header{
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  font-size: larger;
+  font-weight: bold;
+}
+
+.schedule-times{
+  font-family: 'Times New Roman', Times, serif;
+  font-size: large;
+}
+</style>
