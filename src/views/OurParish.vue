a5e519160896b5a7c0977757e8ca874e7a6ab334
dsudduthb <contactdanielbillingsley@gmail.com>
2026-03-28 16:55:07 -0500

    add(parish life photos): photo carousel about parish life added.

--- src/views/OurParish.vue
+++ src/views/OurParish.vue
@@ -1,5 +1,11 @@
 <script setup>
 import parish_photo2 from '../assets/parish_photo2.png'
+const modules = import.meta.glob('/src/assets/parish_life/*', {
+  eager: true,
+  import: 'default'
+})
+
+const images = Object.values(modules)
 </script>
 <template>
 <v-img :src="parish_photo2" cover>
@@ -11,25 +17,40 @@ import parish_photo2 from '../assets/parish_photo2.png'
     </div>
 </v-img>
 
-<v-container class="ma-10">
+<v-container class="ma-10 mx-auto" width="50%">
     <div class="about-text pa-10 ma-10">
         Hidden in the north-west corner of Oklahoma City is our little parish. We are a diverse group: accountants, engineers, soldiers
         students, nurses, teachers, and more. But we all come together in the beautiful english tradition of the worship of the one, true God.
     </div>
-
-
+</v-container>
+<v-container width="80%">
     <v-divider>
         <div class="about-text">
-            Our Building
+            Parish Life
         </div>
     </v-divider>
-    <v-row class="mt-10">
-        <v-col>
-            <v-img :src="parish_photo" :width="500"/>
-        </v-col>
-    </v-row>
-    
 </v-container>
+<v-container width="50%">
+
+  <v-carousel
+    max-width="700"
+    height="500"
+    hide-delimiter-background
+    show-arrows="hover"
+  >
+    <v-carousel-item
+      v-for="(image, i) in images"
+      :key="i"
+    >
+      <v-img
+        :src="image"
+        contain
+        height="100%"
+      />
+    </v-carousel-item>
+  </v-carousel>
+</v-container>
+
 </template>
 <style>
 .about-text{
@@ -38,5 +59,9 @@ import parish_photo2 from '../assets/parish_photo2.png'
     font-size: x-large;
     font-style: italic;
     text-align: center;
+}
+.v-carousel .v-btn {
+  background-color: #993939 !important;
+  color: white;
 }
 </style>
