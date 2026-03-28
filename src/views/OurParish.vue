c551aa83476954d63d4db7da20ae8d6bffdb16f9
dsudduthb <contactdanielbillingsley@gmail.com>
2026-03-28 14:15:45 -0500

    add(title card): added title card with text.

--- src/views/OurParish.vue
+++ src/views/OurParish.vue
@@ -2,7 +2,12 @@
 import parish_photo from '../assets/parish_photo.png'
 </script>
 <template>
-<v-img :src="parish_photo"/>
-
-
+<v-img :src="parish_photo" cover>
+    <div class="text-h2 text-white overlay-text" style="font-family: 'Times New Roman', Times, serif; text-align: center;">
+       "One thing have I desired of the Lord, that will I seek after; 
+       that I may dwell in the house of the Lord all the days of my life, 
+       to behold the beauty of the Lord, and to enquire in his temple."<br>
+        - Psalm 27:4
+    </div>
+</v-img>
 </template>
