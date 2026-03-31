06a4b00e9808749c44e778ecfe6c080cfafbddf7
dsudduthb <contactdanielbillingsley@gmail.com>
2026-03-30 19:48:09 -0500

    honestly dont know what this is, I just wanna rebase

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,24 +1,47 @@
 <script setup>
     import holyCross from '../assets/holy_cross.png'
+    import { useRouter, useRoute } from 'vue-router'
+    import { computed } from 'vue'
+
+    const router = useRouter()
+    const route = useRoute()
+    const isHome = computed(() => (
+      route.path === '/' || route.path === '/our_parish'
+    ))
+
+    function goToHome(){
+      router.push('/')
+    }
 </script>
 
 <template>
   <v-card
-    class="navbar pa-5 bg-white text-white rounded-0 d-flex flex-column align-center"
+    class="navbar pa-5 rounded-0 d-flex flex-column align-center elevation-0"
+    :class="{
+      'bg-transparent': isHome,
+      'navbar--solid bg-background': !isHome,
+      'navbar--absolute': isHome,
+    }"
   >
-    <img
-      :src="holyCross"
-      alt="Holy Cross"
-      width="70"
-      class="mb-3"
-    />
+    <div class="logo-container">
+      <div class="holy_cross_logo" :class="{'nav-link--white' : isHome}">Holy</div>
+
+      <img
+        :src="holyCross"
+        alt="Holy Cross"
+        width="70"
+        @click="goToHome"
+      />
+
+      <div class="holy_cross_logo" :class="{'nav-link--white' : isHome}">Cross</div>
+    </div>
 
     <div class="d-flex ga-4">
-      <router-link class="nav-link" to="/">HOME</router-link>
-      <router-link class="nav-link" to="/about">ABOUT</router-link>
-      <router-link class="nav-link" to="/worship">WORSHIP</router-link>
-      <router-link class="nav-link" to="/our_parish">OUR PARISH</router-link>
-      <router-link class="nav-link" to="/staff">STAFF</router-link>
+      <router-link class="nav-link" :class="{'nav-link--white' : isHome}" to="/">HOME</router-link>
+      <router-link class="nav-link" :class="{'nav-link--white' : isHome}" to="/about">ABOUT</router-link>
+      <router-link class="nav-link" :class="{'nav-link--white' : isHome}" to="/worship">WORSHIP</router-link>
+      <router-link class="nav-link" :class="{'nav-link--white' : isHome}" to="/our_parish">OUR PARISH</router-link>
+      <router-link class="nav-link" :class="{'nav-link--white' : isHome}" to="/staff">STAFF</router-link>
     </div>
   </v-card>
 </template>
@@ -34,5 +57,38 @@
 .nav-link:hover {
   text-decoration: underline;
   color: #993939;
+}
+
+.logo-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+}
+
+.holy_cross_logo {
+  display: flex;
+  align-items: center;
+  height: 70px;
+  color:black;
+  font-family: "Celtic Font";
+  font-size: x-large;
+}
+
+.navbar--absolute {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+}
+
+.navbar--solid {
+  position: relative; /* or static */
+  width: 100%;
+}
+
+.nav-link--white{
+  color:white;
 }
 </style>
