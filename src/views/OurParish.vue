40740c4b657b7ae47eeedda3e6cc476660bcb493
dsudduthb <contactdanielbillingsley@gmail.com>
2026-05-04 19:42:55 -0500

    I changed some things

--- src/views/OurParish.vue
+++ src/views/OurParish.vue
@@ -13,15 +13,11 @@ const modules = import.meta.glob('/src/assets/parish_life/*', {
   import: 'default'
 })
 
-function goto_apck_site(){
-  window.open("https://www.anglicanpck.org/", "_blank");
-}
-
 const images = Object.values(modules)
 </script>
 <template>
-<v-img :src="parish_photo2" cover class="hero-image">
-  <div class="hero-text">
+<v-img :src="parish_photo2" cover>
+    <div class="text-h2 text-white overlay-text" style="font-family: 'Times New Roman', Times, serif; text-align: center;">
     "One thing have I desired of the Lord, that will I seek after; 
     that I may dwell in the house of the Lord all the days of my life, 
     to behold the beauty of the Lord, and to enquire in his temple."<br>
