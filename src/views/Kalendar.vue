df1e404d96e53415f4e38855aa054f45035d9224
dsudduthb <contactdanielbillingsley@gmail.com>
2026-07-03 08:28:30 -0500

    update(calendar): to include month

--- src/views/Kalendar.vue
+++ src/views/Kalendar.vue
@@ -5,9 +5,9 @@ import MediaBar from '../components/MediaBar.vue'
 </script>
 <template>
 <div align="center" class="calendar-container">
-    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&showNav=0&showTitle=0&showTabs=0&showCalendars=0&src=OGU0YTljYjU2YTJmZWRhMDZhNTAyZGI2MDIxZGI4YmI0ODA5YmFkODBkMGFmNjJkZGI5ODZkYWUyODY1MTJjOUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238e24aa" 
-        style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"
-    ></iframe>
+  <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&showTitle=0&showTabs=0&showTz=0&src=OGU0YTljYjU2YTJmZWRhMDZhNTAyZGI2MDIxZGI4YmI0ODA5YmFkODBkMGFmNjJkZGI5ODZkYWUyODY1MTJjOUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238e24aa" 
+    style="border-width:0" width="800" height="600" frameborder="0" scrolling="no">
+  </iframe>
 </div>
 
 <MassTimes/>
